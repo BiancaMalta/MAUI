5ab86251d58510e8d9f3e455ad8d20d06eb93ab4
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2025-03-23 12:12:07 -0300

    Update NovoProduto.xaml.cs

--- Projeto05/MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ Projeto05/MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,31 +1,38 @@
 using MauiAppMinhasCompras.Models;
- 
- namespace MauiAppMinhasCompras.Views;
- 
- public partial class NovoProduto : ContentPage
- {
- 	public NovoProduto()
- 	{
- 		InitializeComponent();
- 	}
- 
-     private async void ToolbarItem_Clicked(object sender, EventArgs e)
-     {
- 		try
- 		{
- 			Produto p = new Produto
- 			{
- 				Descricao = txt_descricao.Text,
- 				Quantidade = Convert.ToDouble(txt_quantidade.Text),
- 				Preco = Convert.ToDouble(txt_preco.Text)
- 			};
- 
- 			await App.Db.Insert(p);
- 			await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
- 
- 		} catch(Exception ex)
- 		{
- 			await DisplayAlert("Ops", ex.Message, "OK");
- 		}
-     }
- }
+using MauiAppMinhasCompras.ViewModels;
+using Microsoft.Maui.Controls;
+
+namespace MauiAppMinhasCompras.Views
+{
+    public partial class NovoProduto : ContentPage
+    {
+        private ListaProdutoViewModel _viewModel;
+
+        public NovoProduto(ListaProdutoViewModel viewModel)
+        {
+            InitializeComponent();
+            _viewModel = viewModel;
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Produto p = new Produto
+                {
+                    Descricao = txt_descricao.Text,
+                    Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                    Preco = Convert.ToDouble(txt_preco.Text)
+                };
+
+                await _viewModel.AdicionarProduto(p);
+                await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+                await Navigation.PopAsync(); // Fecha a tela ao salvar
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}

afb765fe373bf18b1c501fc40082b7b10389de15
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2025-03-23 12:29:30 -0300

    Update EditarProduto.xaml.cs

--- Projeto05/MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
+++ Projeto05/MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -1,9 +1,31 @@
-namespace MauiAppMinhasCompras.Views;
+using MauiAppMinhasCompras.Models;
+using Microsoft.Maui.Controls;
 
-public partial class EditarProduto : ContentPage
+namespace MauiAppMinhasCompras.Views
 {
-	public EditarProduto()
-	{
-		InitializeComponent();
-	}
+    public partial class EditarProduto : ContentPage
+    {
+        private Produto _produto;
+
+        public EditarProduto(Produto produto)
+        {
+            InitializeComponent();
+            _produto = produto;
+            BindingContext = _produto;
+        }
+
+        private async void ToolbarItem_Salvar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await App.Db.Update(_produto);
+                await DisplayAlert("Sucesso!", "Produto atualizado.", "OK");
+                await Navigation.PopAsync(); // Volta para a tela anterior
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+    }
 }

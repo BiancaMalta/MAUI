6284714d07c2b5b7b2bdd75161479520e89d360b
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2025-03-23 12:11:30 -0300

    Update ListaProduto.xaml.cs

--- Projeto05/MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ Projeto05/MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,26 +1,29 @@
 using MauiAppMinhasCompras.ViewModels;
+using MauiAppMinhasCompras.Models;
 using Microsoft.Maui.Controls;
+using System.Threading.Tasks;
 
 namespace MauiAppMinhasCompras.Views
 {
     public partial class ListaProduto : ContentPage
     {
+        private ListaProdutoViewModel _viewModel;
+
         public ListaProduto()
         {
             InitializeComponent();
-            BindingContext = new ListaProdutoViewModel();
+            _viewModel = new ListaProdutoViewModel();
+            BindingContext = _viewModel;
         }
 
         private void ToolbarItem_Clicked(object sender, EventArgs e)
         {
-            try
-            {
-                Navigation.PushAsync(new Views.NovoProduto());
-            }
-            catch (Exception ex)
-            {
-                DisplayAlert("Ops", ex.Message, "OK");
-            }
+            Navigation.PushAsync(new NovoProduto(_viewModel));
+        }
+
+        private void SomarPrecos(object sender, EventArgs e)
+        {
+            DisplayAlert("Total", $"O total dos produtos é: R$ {_viewModel.TotalPreco:F2}", "OK");
         }
     }
 }

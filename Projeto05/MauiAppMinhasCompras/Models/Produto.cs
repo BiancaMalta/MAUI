521828630266bc312c71201c4823952bdb8b350c
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2025-03-23 12:47:06 -0300

    Update Produto.cs

--- Projeto05/MauiAppMinhasCompras/Models/Produto.cs
+++ Projeto05/MauiAppMinhasCompras/Models/Produto.cs
@@ -1,13 +1,42 @@
-﻿using SQLite;
+using System.ComponentModel;
 
 namespace MauiAppMinhasCompras.Models
 {
-    public class Produto
+    public class Produto : INotifyPropertyChanged
     {
-        [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
+        private double _quantidade;
+        private double _preco;
+
         public string Descricao { get; set; }
-        public double Quantidade {  get; set; }
-        public double Preco {  get; set; }
+
+        public double Quantidade
+        {
+            get => _quantidade;
+            set
+            {
+                _quantidade = value;
+                OnPropertyChanged(nameof(Quantidade));
+                OnPropertyChanged(nameof(TotalPreco));
+            }
+        }
+
+        public double Preco
+        {
+            get => _preco;
+            set
+            {
+                _preco = value;
+                OnPropertyChanged(nameof(Preco));
+                OnPropertyChanged(nameof(TotalPreco));
+            }
+        }
+
+        public double TotalPreco => Quantidade * Preco;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string nome)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nome));
+        }
     }
 }

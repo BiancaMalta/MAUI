01f49480461d79aaad72ab3e933cceefbd40a514
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2025-03-23 11:08:09 -0300

    Liguei a página ao ListaProdutoViewModel.

--- Projeto05/MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
+++ Projeto05/MauiAppMinhasCompras/Views/ListaProduto.xaml.cs
@@ -1,21 +1,26 @@
-namespace MauiAppMinhasCompras.Views;
- 
- public partial class ListaProduto : ContentPage
- {
- 	public ListaProduto()
- 	{
- 		InitializeComponent();
- 	}
- 
-     private void ToolbarItem_Clicked(object sender, EventArgs e)
-     {
- 		try
- 		{
- 			Navigation.PushAsync(new Views.NovoProduto());
- 
- 		} catch (Exception ex)
- 		{
- 			DisplayAlert("Ops", ex.Message, "OK");
- 		}
-     }
- }
+using MauiAppMinhasCompras.ViewModels;
+using Microsoft.Maui.Controls;
+
+namespace MauiAppMinhasCompras.Views
+{
+    public partial class ListaProduto : ContentPage
+    {
+        public ListaProduto()
+        {
+            InitializeComponent();
+            BindingContext = new ListaProdutoViewModel();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new Views.NovoProduto());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+    }
+}

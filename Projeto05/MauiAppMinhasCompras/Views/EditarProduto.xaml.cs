03c658cf0aa27e4e07687327c77b8c55d365f09a
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2025-04-27 13:03:18 -0300

    Update EditarProduto.xaml.cs

--- Projeto05/MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
+++ Projeto05/MauiAppMinhasCompras/Views/EditarProduto.xaml.cs
@@ -1,31 +1,35 @@
 using MauiAppMinhasCompras.Models;
-using Microsoft.Maui.Controls;
-
-namespace MauiAppMinhasCompras.Views
-{
-    public partial class EditarProduto : ContentPage
-    {
-        private Produto _produto;
-
-        public EditarProduto(Produto produto)
-        {
-            InitializeComponent();
-            _produto = produto;
-            BindingContext = _produto;
-        }
-
-        private async void ToolbarItem_Salvar_Clicked(object sender, EventArgs e)
-        {
-            try
-            {
-                await App.Db.Update(_produto);
-                await DisplayAlert("Sucesso!", "Produto atualizado.", "OK");
-                await Navigation.PopAsync(); // Volta para a tela anterior
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Erro", ex.Message, "OK");
-            }
-        }
-    }
-}
+ 
+ namespace MauiAppMinhasCompras.Views;
+ 
+ public partial class EditarProduto : ContentPage
+ {
+     public EditarProduto()
+     {
+         InitializeComponent();
+     }
+ 
+     private async void ToolbarItem_Clicked(object sender, EventArgs e)
+     {
+         try
+         {
+             Produto produto_anexado = BindingContext as Produto;
+ 
+             Produto p = new Produto
+             {
+                 Id = produto_anexado.Id,
+                 Categoria = txt_categoria.Text,
+                 Descricao = txt_descricao.Text,
+                 Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                 Preco = Convert.ToDouble(txt_preco.Text)
+             };
+ 
+             await App.Db.Update(p);
+             await DisplayAlert("Sucesso!", "Registro Atualizado", "OK");
+             await Navigation.PopAsync();
+         }
+         catch (Exception ex)
+         {
+             await DisplayAlert("Ops", ex.Message, "OK");
+         }
+     }
